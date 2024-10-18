5851e99ea8cf3394b050b9b18fbff09290be411a
nauvalachid <nauvalachid@gmail.com>
2024-10-18 08:47:04 +0700

    Menambahkan gambar logo

--- app/src/main/java/com/example/tugas1/MainActivity.kt
+++ app/src/main/java/com/example/tugas1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,7 +47,11 @@ fun BasicLayout(modifier: Modifier = Modifier){
             style = TextStyle(fontSize = 30.sp,
                 fontWeight = FontWeight.Bold))
         Text(text = "Ini Adalah Halaman Login")
-
+        Image(painter = painterResource(
+            id = R.drawable.mu
+        ),
+            contentDescription = ""
+        )
     }
 }
 
