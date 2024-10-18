75e30c0d85d642a52b7e66f843796cd01358d10b
nauvalachid <nauvalachid@gmail.com>
2024-10-18 08:38:58 +0700

    Menambahkan text "LOGIN"

--- app/src/main/java/com/example/tugas1/MainActivity.kt
+++ app/src/main/java/com/example/tugas1/MainActivity.kt
@@ -13,7 +13,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.tugas1.ui.theme.Tugas1Theme
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +41,10 @@ fun BasicLayout(modifier: Modifier = Modifier){
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize()
     ){
-
+        Text(text = "LOGIN",
+            style = TextStyle(fontSize = 30.sp,
+                fontWeight = FontWeight.Bold)
+        )
     }
 }
 
